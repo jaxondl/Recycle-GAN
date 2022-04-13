56838fd08efc65447ecf112de82b7ff51f7cd8a0
Jackson Lightfoot <jaxon.lightfoot@utexas.edu>
2022-04-13 02:06:23 -0500

    Added printing to output construction for progress tracking

--- construct_output.py
+++ construct_output.py
@@ -9,6 +9,8 @@ def construct_output(results_dir, name):
     vid_dir = os.path.join(results_dir, name, 'test_latest', 'videos')
     if not os.path.exists(vid_dir):
         os.makedirs(vid_dir)
+    print(f'Frame directory: {frm_dir}')
+    print(f'Video directory: {vid_dir}')
 
     # make video writer objects for each type
     video_writers = {}
@@ -18,9 +20,11 @@ def construct_output(results_dir, name):
         video_writers[vid_type] = cv2.VideoWriter(os.path.join(vid_dir, f'{prefix}_{vid_type}.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), 30, (256, 256))
 
     # write frames to output videos
-    for frm_name in sorted(os.listdir(frm_dir)):
+    for i, frm_name in enumerate(sorted(os.listdir(frm_dir))):
         for vid_type in vid_types:
             if vid_type in frm_name and frm_name[:len(prefix)] == prefix:
+                if i % (25) == 0:
+                    print(f'Frame: {frm_name}')
                 frm = cv2.imread(os.path.join(frm_dir, frm_name))
                 video_writers[vid_type].write(frm)
 
